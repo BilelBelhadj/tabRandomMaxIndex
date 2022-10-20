d9747235d7f8aa7686363df6f7c4de5d9efc6900
Bilel Belhadj <belhadj19bi@gmail.com>
2022-10-20 09:36:05 -0300

    methode de remplissage et affichage de tableau fonctionels

---
+++ src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        PlusGrandValeur tab1 = new PlusGrandValeur();
+        tab1.display();
+    }
+}
---
+++ src/PlusGrandValeur.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class PlusGrandValeur {
+
+    //declaration des attributs
+    int[] vals = {};
+    //initialisatio de taille de tableau
+    final int TAILLE = 15;
+
+    //constructeur pour remplir le tableau
+    PlusGrandValeur(){
+
+        //initialisation necessaire de tabbleau
+        vals = new int[TAILLE];
+
+        //remplissage de taleau avec des valeur aleatoire
+        for(int i = 0; i < TAILLE; i++){
+            Random rand = new Random();
+            vals[i] = rand.nextInt(101);
+        }
+    }
+
+    //methode d affichage de max et son index
+    public void display(){
+
+        for(int i = 0; i < TAILLE; i++){
+            System.out.println(vals[i]);
+        }
+    }
+}
